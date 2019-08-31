5efb38e10bec6a837d55526f8416fc89b436a865
V Saravanan <saravanan.resume@gmail.com>
2019-08-31 17:49:08 +0800

    Added Table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {Component}  from 'react';
+import Table from './Table';
 
 
 class App extends Component {
     render() {
       return (
         <div>
-          <h1>Hello, React!</h1>
+          <Table/>
         </div>
       )
     }
