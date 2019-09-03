89d63fa9ca6cdcfb13538c9d7a6b722c55adb2e6
V Saravanan <saravanan.resume@gmail.com>
2019-09-03 16:25:13 +0800

    added extensions

--- src/Form.js
+++ src/Form.js
@@ -1,16 +1,9 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
-    constructor (props) {
-        super(props)
-
-        this.initialState = {
-            name: '',
-            job: ''
-        }
-
-        this.state = this.initialState;
-
+    state = {
+        name: '',
+        job: ''
     }
 
     handleChange =  event => {
